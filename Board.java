53cacd6fcfddadf0938f0bd53e44ccea5c3f7fae
TobyTuck <tobyjtuckness@gmail.com>
2022-09-27 22:10:44 -0700

    Beta Version

--- Board.java
+++ Board.java
@@ -5,7 +5,69 @@ import java.awt.event.*;
 public class Board extends JFrame 
                    implements ActionListener{
 
+    // JPanel that consists of the entire 8 * 8 chessboard
     private JPanel board;
+    // individual squares of the chessboard
+    private JPanel square1;
+    private JPanel square2;
+    private JPanel square3;
+    private JPanel square4;
+    private JPanel square5;
+    private JPanel square6;
+    private JPanel square7;
+    private JPanel square8;
+    private JPanel square9;
+    private JPanel square10;
+    private JPanel square11;
+    private JPanel square12;
+    private JPanel square13;
+    private JPanel square14;
+    private JPanel square15;
+    private JPanel square16;
+    private JPanel square17;
+    private JPanel square18;
+    private JPanel square19;
+    private JPanel square20;
+    private JPanel square21;
+    private JPanel square22;
+    private JPanel square23;
+    private JPanel square24;
+    private JPanel square25;
+    private JPanel square26;
+    private JPanel square27;
+    private JPanel square28;
+    private JPanel square29;
+    private JPanel square30;
+    private JPanel square31;
+    private JPanel square32;
+    private JPanel square33;
+    private JPanel square34;
+    private JPanel square35;
+    private JPanel square36;
+    private JPanel square37;
+    private JPanel square38;
+    private JPanel square39;
+    private JPanel square40;
+    private JPanel square41;
+    private JPanel square42;
+    private JPanel square43;
+    private JPanel square44;
+    private JPanel square45;
+    private JPanel square46;
+    private JPanel square47;
+    private JPanel square48;
+    private JPanel square49;
+    private JPanel square50;
+    private JPanel square51;
+    private JPanel square52;
+    private JPanel square53;
+    private JPanel square54;
+    private JPanel square55;
+    private JPanel square56;
+    private JPanel square57;
+    private JPanel square58;
+    private JPanel square59;
+    private JPanel square60;
 
     // No-arg constructor that  creates the board
     public Board(){
@@ -18,6 +80,9 @@ public class Board extends JFrame
         this.setSize(screenSize.width, screenSize.height);
         this.setLayout(new BorderLayout());
         getContentPane().setBackground(Color.green);
+        this.setVisible(true);
 
-        board = new JPanel(new FlowLayout(CENTER(), 0, 0);
+        // build the board
+        board = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        board.setPreferredSize(screenSize.width * 0.75, screenSize.height * 0.75);
 }
