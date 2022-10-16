8d23f3daf592b9b607f90647a175a997b9938171
TobyTuck <tobyjtuckness@gmail.com>
2022-10-15 23:36:08 -0700

    10/15 edit

--- Delete.java
+++ Delete.java
@@ -1,5 +1,14 @@
 public class Delete{
 
     public static void main(String[] args){
-    ChessBoard board = new ChessBoard();}
+    ChessBoard board = new ChessBoard();
+    
+    System.out.println("Chess cell dimensions: " + board.getCellDimension() + "\n" +
+                       "Chess Board dimensions: " + board.getBoardDimension() + "\n" +
+                       "Chess North dimensions: " + board.getNorthDimension() + "\n" +
+                       "Chess South dimensions: " + board.getSouthDimension() + "\n" +
+                       "Chess West dimensions: " + board.getWestDimension() + "\n" +
+                       "Chess East dimensions: " + board.getEastDimension() + "\n" +
+                       "Program Window dimensions: " + board.getWindowSize());
     }
+}
