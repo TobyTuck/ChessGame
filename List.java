e76ab07f866cdfb279af735b71fb013124aa47ea
TobyTuck <tobyjtuckness@gmail.com>
2022-10-30 22:40:10 -0700

    10/30 edit

--- List.java
+++ List.java
@@ -61,13 +61,19 @@ public class List{
         Method that retrieves the data at a specified location in the structure w/o deletion
     */
     public Object pop(int target){
+        // special case- target is larger than the size of the list
+        if(_size < target)
+            return null;
+
         Link tail = _head;
         int lowerLimit = target / _layerWidth;
         int upperLimit = lowerLimit + 1;
+        int a = _size / _layerWidth;
 
         // instance where the shortest route to the target is from the 1st node of the same row 
-        if((target - (lowerLimit * _layerWidth)) < ((upperLimit * _layerWidth) - target)){
-            for(int index = 0; index < lowerLimit; ++index){
+        if(((target - (lowerLimit * _layerWidth)) < ((upperLimit * _layerWidth) - target)) || 
+            upperLimit == a){
+            for(int index = 0; index < lowerLimit - 1; ++index){
                 tail = tail.getDown();}
 
             for(int index = 0; index < (target % _layerWidth); ++index){
